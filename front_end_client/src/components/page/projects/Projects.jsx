45d24cf0cd9b77ca138f6a52c4bbc6bc4dbd6e45
SatrioHalim <satriohalim21@gmail.com>
2026-05-18 14:02:23 +0700

    Feat : Search feature

--- front_end_client/src/components/page/projects/Projects.jsx
+++ front_end_client/src/components/page/projects/Projects.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Link,
+  Stack,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
@@ -10,28 +11,42 @@ import SidebarLayout from '@/components/layouts/sidebarlayout';
 import Table from '@/components/ui/table';
 import services from '@/services';
 import datetime from '@/utils/datetime';
+import TextField from '@/components/ui/forms/textfield';
+import { useDebounce } from 'use-debounce'
 
 const Projects = () => {
   // loading
   const [isLoading, setLoading] = useState(false);
   const [boardsData, setBoardsData] = useState([]);
 
-  const { control } = useForm({
-    defaultValues: {
+  
+  const {control} = useForm({
+    defaultValues:{
       search: '',
-    },
+    }
   });
+
+  const watchSearch = useWatch({
+    control,
+    name: 'search',
+  });
+
+  const [debouncedSearch] = useDebounce(watchSearch, 1000); // debounce 1 detik
+
+  console.log('debounced search', debouncedSearch);
 
   useEffect(() => {
     const fetchBoardsData = async () => {
       setLoading(true);
-      const response = await services.boards.myBoards();
+      const response = await services.boards.myBoards({
+        filter: debouncedSearch,
+      });
       setBoardsData(response.data.data);
       setLoading(false);
     };
 
     fetchBoardsData();
-  }, []); // onMounted
+  }, [debouncedSearch]); // onMounted
 
   return (
     <SidebarLayout
@@ -42,6 +57,17 @@ const Projects = () => {
         },
       ]}
     >
+      <Stack>
+        <Box>
+          <TextField
+          control={control}
+            label={"Find project name"}
+            id={"search"}
+            name={"search"}
+            size={"small"}
+          ></TextField>
+        </Box>
+      </Stack>
       <Table
         isLoading={isLoading}
         data={boardsData}
