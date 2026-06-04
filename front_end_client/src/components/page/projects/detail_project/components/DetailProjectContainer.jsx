056a8d989ddae4484d91622d5de0cd1db3e06b44
SatrioHalim <satriohalim21@gmail.com>
2026-06-04 16:22:49 +0700

    Feat : Add hooks useCreateNewList

--- front_end_client/src/components/page/projects/detail_project/components/DetailProjectContainer.jsx
+++ front_end_client/src/components/page/projects/detail_project/components/DetailProjectContainer.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Link } from '@mui/material';
+import { Box, Button, colors, Link, Paper, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 
@@ -6,76 +6,31 @@ import SidebarLayout from '@/components/layouts/sidebarlayout';
 import Table from '@/components/ui/table';
 import services from '@/services';
 import datetime from '@/utils/datetime';
+import { href, useLoaderData } from 'react-router';
+import useDetailProjectContext from '../hooks/useDetailProjectContext';
 
 const DetailProjectContainer = () => {
   // loading
-  const [isLoading, setLoading] = useState(false);
-  const [boardsData, setBoardsData] = useState([]);
+  // const [isLoading, setLoading] = useState(false);
+  // const [boardsData, setBoardsData] = useState([]);
 
-  const { control } = useForm({
-    defaultValues: {
-      search: '',
-    },
-  });
-
-  useEffect(() => {
-    const fetchBoardsData = async () => {
-      setLoading(true);
-      const response = await services.boards.myBoards({
-        filter: debounceSearch,
-      });
-      setBoardsData(response.data.data);
-      setLoading(false);
-    };
-
-    fetchBoardsData();
-  }, []); // onMounted
+  const detailProjectData = useLoaderData();
+  const detailProjectContext = useDetailProjectContext();
 
   return (
     <SidebarLayout
-      pageTitle="Project Detail"
+      pageTitle={`${detailProjectData.title} (${detailProjectContext.getProjectInitials})`}
       breadcrumbs={[
         {
           label: 'Project List',
+          href:'/projects',
         },
+        {
+          label: detailProjectData.title,
+          href: `/projects/${detailProjectData.public_id}`,
+        }
       ]}
-    >
-      <Table
-        isLoading={isLoading}
-        data={boardsData}
-        columns={[
-          {
-            id: 'title',
-            label: 'Nama proyek',
-          },
-          {
-            id: 'description',
-            label: 'Deskripsi',
-          },
-          {
-            id: 'title',
-            label: 'Tanggal dibuat',
-            render(data) {
-              return (
-                <Box>{datetime.format(data.created_at, 'DD/MM/YYYY')}</Box>
-              );
-            },
-          },
-          {
-            id: 'title',
-            label: 'Aksi',
-            render(data) {
-              return (
-                <Link to={`/projects/${data.public_id}`}>
-                  <Button type="button" variant="outlined">
-                    Detail proyek
-                  </Button>
-                </Link>
-              );
-            },
-          },
-        ]}
-      />
+    > 
     </SidebarLayout>
   );
 };
