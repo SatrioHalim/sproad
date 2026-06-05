4d92e64f416c795137867f61ed725e48fb1b7053
SatrioHalim <satriohalim21@gmail.com>
2026-06-05 14:54:25 +0700

    Style : formatting

--- front_end_client/src/components/page/projects/detail_project/hooks/useListSortableItem.js
+++ front_end_client/src/components/page/projects/detail_project/hooks/useListSortableItem.js
@@ -1,65 +1,61 @@
-import { useState } from "react";
-import useDetailProjectContext from "./useDetailProjectContext"
-import { useDroppable } from "@dnd-kit/core";
-import { DRAG_LIST } from "@/utils/constants";
-import { useSortable } from "@dnd-kit/sortable";
-import services from "@/services";
+import { useDroppable } from '@dnd-kit/core';
+import { useSortable } from '@dnd-kit/sortable';
+import { useState } from 'react';
 
-const useListSortableItem = ({id,item}) => {
-    const detailProjectContext = useDetailProjectContext();
-    const taskItems = detailProjectContext.getTaskItemsByListId(item.public_id);
+import useDetailProjectContext from './useDetailProjectContext';
 
-    const [isShowConfirmDelete, setShowConfirmDelete] = useState(false);
+import services from '@/services';
+import { DRAG_LIST } from '@/utils/constants';
 
-    const {
-        setNodeRef: setNodeRefDroppable,
-        isOver,
-        active,
-        over
-    } = useDroppable({
-        id,
-        data:{
-            ...item,
-            type:DRAG_LIST
-        }
+const useListSortableItem = ({ id, item }) => {
+  const detailProjectContext = useDetailProjectContext();
+  const taskItems = detailProjectContext.getTaskItemsByListId(item.public_id);
+
+  const [isShowConfirmDelete, setShowConfirmDelete] = useState(false);
+
+  const {
+    setNodeRef: setNodeRefDroppable,
+    isOver,
+    active,
+    over,
+  } = useDroppable({
+    id,
+    data: {
+      ...item,
+      type: DRAG_LIST,
+    },
+  });
+
+  const { setNodeRef, attributes, listeners, transform, transition } =
+    useSortable({
+      id,
+      data: {
+        ...item,
+        type: DRAG_LIST,
+      },
     });
+  const handleDeleteList = (listId) => async (event) => {
+    await services.lists.remove(listId);
+    await detailProjectContext.fetchBoardLists();
+    setShowConfirmDelete(false);
+  };
 
-    const {
-        setNodeRef,
-        attributes,
-        listeners,
-        transform,
-        transition
-    } = useSortable({
-        id,
-        data :{
-            ...item,
-            type:DRAG_LIST
-        }
-    });
-    const handleDeleteList = (listId) => async (event) => {
-            await services.lists.remove(listId);
-            await detailProjectContext.fetchBoardLists();
-            setShowConfirmDelete(false);
-        }
-    
-        return {
-            detailProjectContext,
-            taskItems,
-            isOver,
-            active,
-            over,
-            setNodeRefDroppable,
-            attributes,
-            listeners,
-            setNodeRef,
-            transform,
-            transition,
-            handleDeleteList,
-            isShowConfirmDelete,
-            setShowConfirmDelete
-        }
+  return {
+    detailProjectContext,
+    taskItems,
+    isOver,
+    active,
+    over,
+    setNodeRefDroppable,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    handleDeleteList,
+    isShowConfirmDelete,
+    setShowConfirmDelete,
+  };
+};
 
-}
-
-export default useListSortableItem
+export default useListSortableItem;
