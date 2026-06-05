9575cddfd00a594ad613056517b1d0bc69db76f4
SatrioHalim <satriohalim21@gmail.com>
2026-06-05 14:02:54 +0700

    Style : formatting

--- front_end_client/src/components/page/projects/detail_project/hooks/useCreateNewTask.js
+++ front_end_client/src/components/page/projects/detail_project/hooks/useCreateNewTask.js
@@ -1,66 +1,64 @@
-import { useMemo, useState } from "react";
-import useDetailProjectContext from "./useDetailProjectContext"
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import services from "@/services";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import useDetailProjectContext from './useDetailProjectContext';
+
+import services from '@/services';
 
 const createNewTaskSchema = yup.object({
   title: yup.string().required('Title is required'),
 });
 
 const useCreateNewTask = (listId) => {
-    const detailProjectContext = useDetailProjectContext();
-    const taskitemsData = detailProjectContext.getTaskItemsByListId(listId);
+  const detailProjectContext = useDetailProjectContext();
+  const taskitemsData = detailProjectContext.getTaskItemsByListId(listId);
 
-    const taskItemsDataIds = useMemo(()=>{
-        return taskitemsData.map((item) => item.public_id)
-    },[taskitemsData])
+  const taskItemsDataIds = useMemo(() => {
+    return taskitemsData.map((item) => item.public_id);
+  }, [taskitemsData]);
 
-    const [isLoading, setLoading] = useState(false);
-    const [isShowFormCreateNewTask, setShowFormCreateNewTask] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [isShowFormCreateNewTask, setShowFormCreateNewTask] = useState(false);
 
-    const {
-        control,
-        handleSubmit,
-        reset
-    } = useForm({
-        defaultValues:{
-            title: '',
-        },
-        resolver:yupResolver(createNewTaskSchema)
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      title: '',
+    },
+    resolver: yupResolver(createNewTaskSchema),
+  });
+
+  const handleOpenFormCreateNewTask = () => {
+    setShowFormCreateNewTask(true);
+  };
+
+  const handleCloseFormCreateNewTask = () => {
+    setShowFormCreateNewTask(false);
+  };
+
+  const onSubmit = async (values) => {
+    setLoading(true);
+    await services.cards.create({
+      ...values,
+      list_id: listId,
+      position: taskItemsDataIds.length == 0 ? 1 : taskItemsDataIds.length + 1,
     });
+    setLoading(false);
+    reset();
+    handleCloseFormCreateNewTask();
+    await detailProjectContext.fetchBoardLists();
+  };
 
-    const handleOpenFormCreateNewTask = () => {
-        setShowFormCreateNewTask(true);
-    }
+  return {
+    isLoading,
+    isShowFormCreateNewTask,
+    control,
+    handleSubmit,
+    handleOpenFormCreateNewTask,
+    handleCloseFormCreateNewTask,
+    onSubmit,
+  };
+};
 
-    const handleCloseFormCreateNewTask = () => {
-        setShowFormCreateNewTask(false);
-    }
-
-    const onSubmit = async (values) => {
-        setLoading(true);
-        await services.cards.create({
-            ...values,
-            list_id: listId,
-            position: taskItemsDataIds.length == 0 ? 1 : taskItemsDataIds.length + 1
-        });
-        setLoading(false);
-        reset();
-        handleCloseFormCreateNewTask();
-        await detailProjectContext.fetchBoardLists();
-    }
-
-    return {
-        isLoading,
-        isShowFormCreateNewTask,
-        control,
-        handleSubmit,
-        handleOpenFormCreateNewTask,
-        handleCloseFormCreateNewTask,
-        onSubmit
-    }
-}
-
-export default useCreateNewTask
+export default useCreateNewTask;
--- front_end_client/src/components/page/projects/detail_project/hooks/useDetailProjectContainer.js
+++ front_end_client/src/components/page/projects/detail_project/hooks/useDetailProjectContainer.js
@@ -1,74 +1,78 @@
-import { useLoaderData } from "react-router"
-import useDetailProjectContext from "./useDetailProjectContext";
-import { useMemo, useState } from "react";
-import { MouseSensor, useSensor, useSensors } from "@dnd-kit/core";
-import { PointerSensor } from "@dnd-kit/react";
-import { DRAG_LIST } from "@/utils/constants";
+import { MouseSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { PointerSensor } from '@dnd-kit/react';
+import { useMemo, useState } from 'react';
+import { useLoaderData } from 'react-router';
+
+import useDetailProjectContext from './useDetailProjectContext';
+
+import { DRAG_LIST } from '@/utils/constants';
 
 const useDetailProjectContainer = () => {
-    const detailProjectData = useLoaderData();
-    const detailProjectContext = useDetailProjectContext();
+  const detailProjectData = useLoaderData();
+  const detailProjectContext = useDetailProjectContext();
 
-    const [activeDragItem, setActiveDragItem] = useState(null);
-    const boardListData = detailProjectContext.boardListData;
+  const [activeDragItem, setActiveDragItem] = useState(null);
+  const boardListData = detailProjectContext.boardListData;
 
-    const mouseSensor = useSensor(MouseSensor, {
-        activationConstraint:{
-            distance: 10,
-        }
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10,
+    },
+  });
+
+  const pointerSensor = useSensor(PointerSensor, {
+    activationConstraint: {
+      distance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, pointerSensor);
+
+  const handleDragStart = (event) => {
+    setActiveDragItem(event.active.data.current);
+  };
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    setActiveDragItem(null);
+
+    if (!over) return;
+
+    const isDragListType =
+      active.data.current.type == DRAG_LIST &&
+      over.data.current.type === DRAG_LIST;
+
+    const isSameList =
+      active.data.current.list_internal_id ==
+      (over.data.current.list_internal_id || over.data.current.internal_id);
+
+    detailProjectContext.setUpdateTaskItemPosition({
+      active: active.data.current,
+      over: over.data.current,
+      isDragListType,
+      isSameList,
     });
+  };
 
-    const pointerSensor = useSensor(PointerSensor, {
-        activationConstraint:{
-            distance: 5,
-        }
-    })
+  const handleDragCancel = () => {
+    setActiveDragItem(null);
+  };
 
-    const sensors = useSensors(mouseSensor, pointerSensor);
+  const boardListDataMapPublicIds = useMemo(() => {
+    return boardListData.map((item) => item.public_id);
+  }, [boardListData]);
 
-    const handleDragStart = (event) => {
-        setActiveDragItem(event.active.data.current);
-    };
+  return {
+    detailProjectData,
+    detailProjectContext,
+    boardListData,
+    boardListDataMapPublicIds,
+    activeDragItem,
+    sensors,
+    handleDragStart,
+    handleDragEnd,
+    handleDragCancel,
+  };
+};
 
-    const handleDragEnd = (event) => {
-        const {active, over} = event;
-        setActiveDragItem(null);
-
-        if(!over) return;
-        
-        const isDragListType =  active.data.current.type == DRAG_LIST && over.data.current.type === DRAG_LIST;
-        
-        const isSameList =  active.data.current.list_internal_id == (
-            over.data.current.list_internal_id || over.data.current.internal_id
-        );
-
-        detailProjectContext.setUpdateTaskItemPosition({
-            active: active.data.current,
-            over: over.data.current,
-            isDragListType,
-            is
-        });
-    }
-
-    const handleDragCancel = () => {
-        setActiveDragItem(null);
-    }
-
-    const boardListDataMapPublicIds = useMemo(()=> {
-        return boardListData.map((item) => item.public_id);
-    },[boardListData])
-
-    return {
-        detailProjectData,
-        detailProjectContext,
-        boardListData,
-        boardListDataMapPublicIds,
-        activeDragItem,
-        sensors,
-        handleDragStart,
-        handleDragEnd,
-        handleDragCancel
-    }
-}
-
-export default useDetailProjectContainer
+export default useDetailProjectContainer;
