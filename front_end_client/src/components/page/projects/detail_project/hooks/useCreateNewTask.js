58142f49e813b1b96fdf7035c80888cbe221281e
SatrioHalim <satriohalim21@gmail.com>
2026-06-05 10:48:01 +0700

    Feat : Add useCreateNewTask hooks

---
+++ front_end_client/src/components/page/projects/detail_project/hooks/useCreateNewTask.js
@@ -0,0 +1,66 @@
+import { useMemo, useState } from "react";
+import useDetailProjectContext from "./useDetailProjectContext"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import services from "@/services";
+
+const createNewTaskSchema = yup.object({
+  title: yup.string().required('Title is required'),
+});
+
+const useCreateNewTask = (listId) => {
+    const detailProjectContext = useDetailProjectContext();
+    const taskitemsData = detailProjectContext.getTaskItemsByListId(listId);
+
+    const taskItemsDataIds = useMemo(()=>{
+        return taskitemsData.map((item) => item.public_id)
+    },[taskitemsData])
+
+    const [isLoading, setLoading] = useState(false);
+    const [isShowFormCreateNewTask, setShowFormCreateNewTask] = useState(false);
+
+    const {
+        control,
+        handleSubmit,
+        reset
+    } = useForm({
+        defaultValues:{
+            title: '',
+        },
+        resolver:yupResolver(createNewTaskSchema)
+    });
+
+    const handleOpenFormCreateNewTask = () => {
+        setShowFormCreateNewTask(true);
+    }
+
+    const handleCloseFormCreateNewTask = () => {
+        setShowFormCreateNewTask(false);
+    }
+
+    const onSubmit = async (values) => {
+        setLoading(true);
+        await services.cards.create({
+            ...values,
+            list_id: listId,
+            position: taskItemsDataIds.length == 0 ? 1 : taskItemsDataIds.length + 1
+        });
+        setLoading(false);
+        reset();
+        handleCloseFormCreateNewTask();
+        await detailProjectContext.fetchBoardLists();
+    }
+
+    return {
+        isLoading,
+        isShowFormCreateNewTask,
+        control,
+        handleSubmit,
+        handleOpenFormCreateNewTask,
+        handleCloseFormCreateNewTask,
+        onSubmit
+    }
+}
+
+export default useCreateNewTask
