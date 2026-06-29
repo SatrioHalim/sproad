67990f7096a55e98363af8da395af32cbad1d46f
SatrioHalim <satriohalim21@gmail.com>
2026-06-29 15:19:26 +0700

    Style : formatting

--- front_end_client/src/components/page/projects/detail_project/hooks/useTaskItems.js
+++ front_end_client/src/components/page/projects/detail_project/hooks/useTaskItems.js
@@ -1,19 +1,20 @@
-import { useMemo } from "react";
-import useDetailProjectContext from "./useDetailProjectContext";
+import { useMemo } from 'react';
+
+import useDetailProjectContext from './useDetailProjectContext';
 
 const useTaskItems = (listId) => {
-    const detailProjectContext = useDetailProjectContext();
-    const taskItemsData = detailProjectContext.getTaskItemsByListId(listId);
+  const detailProjectContext = useDetailProjectContext();
+  const taskItemsData = detailProjectContext.getTaskItemsByListId(listId);
 
-    const taskitemDataIds = useMemo(()=>{
-        return taskItemsData.map((item) => item.public_id)
-    },[taskItemsData])
+  const taskitemDataIds = useMemo(() => {
+    return taskItemsData.map((item) => item.public_id);
+  }, [taskItemsData]);
 
-    return {
-        detailProjectContext,
-        taskItemsData,
-        taskitemDataIds
-    }
-}
+  return {
+    detailProjectContext,
+    taskItemsData,
+    taskitemDataIds,
+  };
+};
 
 export default useTaskItems;
