925c24d73ccf87113bea107bab7eafa9bcc8001e
SatrioHalim <satriohalim21@gmail.com>
2026-06-29 15:18:05 +0700

    Feat: add TaskItems component

---
+++ front_end_client/src/components/page/projects/detail_project/components/TaskItems.jsx
@@ -0,0 +1,85 @@
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { Box, colors, Paper, Stack, Typography } from "@mui/material";
+import useTaskItems from "../hooks/useTaskItems";
+import TaskSortableItem from "./TaskSortableItem";
+import { DRAG_CARD } from "@/utils/constants";
+
+const TaskItems = ({listDroppable, listItem}) => {
+    const {active} = listDroppable
+    const {
+        taskitemDataIds,
+        taskItemsData
+    } = useTaskItems(listItem.public_id)
+    
+    const renderTaskDropZone = (listItem) => {
+        const isCardDragging = active && active.data.current.list_public_id != listItem.public_id && active.data.current.type == DRAG_CARD
+
+        if(isCardDragging){
+            return (
+                <Box
+                sx={{ 
+                    p:1,
+                    position:'absolute',
+                    width:'100%',
+                    zIndex:1
+                 }}
+                >
+                    <Paper
+                    elevation={0}
+                    sx={{ 
+                        p:1,
+                        bgcolor: colors.blue[50],
+                        borderStyle:'dashed',
+                        borderColor:colors.blue[200]
+                     }}
+                    >
+                        <Stack
+                            sx={{ 
+                                height:800,
+                                alignItems:'center',
+                                justifyContent:'center'
+                             }}
+                        >
+                            <Typography variant="body1" sx={{ 
+                                fontWeight:600
+                             }} color={colors.blue[800]}>
+                                {listItem.title}
+                             </Typography>
+                        </Stack>
+                    </Paper>
+                </Box>
+            )
+        }
+
+        return <></>
+    }
+    
+    return (
+        <Box 
+        sx={{ 
+            height: 850,
+            overflowY:'auto',
+            position:'relative'
+         }}
+         >
+            {renderTaskDropZone()}
+            <Stack 
+            sx={{ gap:2,
+                p:1
+             }}>
+                <SortableContext
+                items={taskitemDataIds}
+                strategy={verticalListSortingStrategy}
+                >
+                    {
+                        taskItemsData?.map((item)=>(
+                            <TaskSortableItem key={item.public_id} id={item.public_id} item={item} listId={listItem.public_id}></TaskSortableItem>
+                        ))
+                    }
+                </SortableContext>
+             </Stack>
+        </Box>
+    )
+}
+
+export default TaskItems;
---
+++ front_end_client/src/components/page/projects/detail_project/hooks/useTaskItems.js
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import useDetailProjectContext from "./useDetailProjectContext";
+
+const useTaskItems = (listId) => {
+    const detailProjectContext = useDetailProjectContext();
+    const taskItemsData = detailProjectContext.getTaskItemsByListId(listId);
+
+    const taskitemDataIds = useMemo(()=>{
+        return taskItemsData.map((item) => item.public_id)
+    },[taskItemsData])
+
+    return {
+        detailProjectContext,
+        taskItemsData,
+        taskitemDataIds
+    }
+}
+
+export default useTaskItems;
